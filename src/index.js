6437bddd69e95bcc32b1fe64295f1909c0502b5a
J Wang <janis.j.wang@gmail.com>
2019-07-28 16:19:55 +1000

    Stage 0 - Initialising project
    Commit includes
    1. update config files, e.g manifest.json
    2. remove unused files.
    3. add node-sass into project.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './components/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>Hello world</div>,
+    document.querySelector('#root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
